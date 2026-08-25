9eb01545e1509746719e7f0da3cc2ed83709d08a
Felipe <fae.20@hotmail.com>
2026-08-24 23:10:06 -0300

    feat(controller): feat statistics controller

--- src/main/java/desafio/itau/springboot/service/TransactionService.java
+++ src/main/java/desafio/itau/springboot/service/TransactionService.java
@@ -24,7 +24,7 @@ public class TransactionService {
     public DoubleSummaryStatistics getStatistics() {
         OffsetDateTime now = OffsetDateTime.now();
         return transactions.stream()
-                .filter(t -> t.getDataHora().isAfter(now.minusSeconds(60)))
+                //.filter(t -> t.getDataHora().isAfter(now.minusSeconds(60)))
                 .mapToDouble(Transaction::getValor)
                 .summaryStatistics();
     }
